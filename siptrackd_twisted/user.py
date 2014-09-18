10ea07cb1e2b6f8dce16d7d932914f12dd3b720b
Simon Ekstrand <simon@ipeer.se>
2014-09-18 13:06:46 +0200

    Updated xmlrpc session validation for user.py

--- siptrackd_twisted/user.py
+++ siptrackd_twisted/user.py
@@ -15,200 +15,158 @@ class UserManagerRPC(baserpc.BaseRPC):
 
 class UserManagerLocalRPC(baserpc.BaseRPC):
     node_type = 'user manager local'
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_add(self, parent_oid):
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_add(self, session, parent_oid):
         """Create a new UserManager."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'user manager local')
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'user manager local')
         return obj.oid
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_delete(self, oid, recursive = True):
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_delete(self, session, oid, recursive = True):
         """Delete a node."""
-        node = self.getOID(oid)
+        node = self.getOID(session, oid)
         node.delete(recursive)
         return True
 
 class UserManagerLDAPRPC(baserpc.BaseRPC):
     node_type = 'user manager ldap'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_add(self, parent_oid, connection_type, server, port, base_dn,
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_add(self, session, parent_oid, connection_type, server, port, base_dn,
             valid_groups):
         """Create a new UserManagerLDAP."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'user manager ldap', connection_type, server, port,
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'user manager ldap', connection_type, server, port,
                 base_dn, valid_groups)
         return obj.oid
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_delete(self, oid, recursive = True):
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_delete(self, session, oid, recursive = True):
         """Delete a node."""
-        node = self.getOID(oid)
+        node = self.getOID(session, oid)
         node.delete(recursive)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_sync_users(self, oid, purge_missing_users):
-        node = self.getOID(oid)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_sync_users(self, session, oid, purge_missing_users):
+        node = self.getOID(session, oid)
         node.syncUsers(purge_missing_users)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_connection_type(self, oid, connection_type):
-        node = self.getOID(oid)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_connection_type(self, session, oid, connection_type):
+        node = self.getOID(session, oid)
         node.setConnectionType(connection_type)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_server(self, oid, server):
-        self.getOID(oid).setServer(server)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_server(self, session, oid, server):
+        self.getOID(session, oid).setServer(server)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_port(self, oid, port):
-        self.getOID(oid).setPort(port)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_port(self, session, oid, port):
+        self.getOID(session, oid).setPort(port)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_base_dn(self, oid, base_dn):
-        self.getOID(oid).setBaseDN(base_dn)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_base_dn(self, session, oid, base_dn):
+        self.getOID(session, oid).setBaseDN(base_dn)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_valid_groups(self, oid, valid_groups):
-        self.getOID(oid).setValidGroups(valid_groups)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_valid_groups(self, session, oid, valid_groups):
+        self.getOID(session, oid).setValidGroups(valid_groups)
         return True
 
 class UserManagerActiveDirectoryRPC(baserpc.BaseRPC):
     node_type = 'user manager active directory'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_add(self, parent_oid, server, base_dn,
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_add(self, session, parent_oid, server, base_dn,
             valid_groups, user_domain):
         """Create a new UserManagerActiveDirectory."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'user manager active directory', server,
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'user manager active directory', server,
                 base_dn, valid_groups, user_domain)
         return obj.oid
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_delete(self, oid, recursive = True):
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_delete(self, session, oid, recursive = True):
         """Delete a node."""
-        node = self.getOID(oid)
+        node = self.getOID(session, oid)
         node.delete(recursive)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_sync_users(self, oid, username, password, purge_missing_users):
-        node = self.getOID(oid)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_sync_users(self, session, oid, username, password, purge_missing_users):
+        node = self.getOID(session, oid)
         node.syncUsers(username, password, purge_missing_users)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_server(self, oid, server):
-        self.getOID(oid).setServer(server)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_server(self, session, oid, server):
+        self.getOID(session, oid).setServer(server)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_base_dn(self, oid, base_dn):
-        self.getOID(oid).setBaseDN(base_dn)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_base_dn(self, session, oid, base_dn):
+        self.getOID(session, oid).setBaseDN(base_dn)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_valid_groups(self, oid, valid_groups):
-        self.getOID(oid).setValidGroups(valid_groups)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_valid_groups(self, session, oid, valid_groups):
+        self.getOID(session, oid).setValidGroups(valid_groups)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_user_domain(self, oid, user_domain):
-        self.getOID(oid).setUserDomain(user_domain)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_user_domain(self, session, oid, user_domain):
+        self.getOID(session, oid).setUserDomain(user_domain)
         return True
 
 class BaseUserRPC(baserpc.BaseRPC):
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_add(self, parent_oid, username, password):
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_add(self, session, parent_oid, username, password):
         """Create a new User."""
         return False
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_delete(self, oid):
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_delete(self, session, oid):
         """Delete a User."""
-        user = self.getOID(oid)
+        user = self.getOID(session, oid)
         user.delete(recursive = True)
         self.session_handler.killUserSessions(user)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_username(self, oid, username):
-        user = self.getOID(oid)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_username(self, session, oid, username):
+        user = self.getOID(session, oid)
         node.setUsername(username)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_set_password(self, oid, new_password, old_password):
-        session_user = self.session.user.user
-        user = self.getOID(oid)
+    @helpers.ValidateSession()
+    def xmlrpc_set_password(self, session, oid, new_password, old_password):
+        session_user = session.user.user
+        user = self.getOID(session, oid)
         if session_user.administrator != True and \
                 session_user.oid != user.oid:
             raise errors.PermissionDenied()
         if old_password is False:
             if session_user.oid != user.oid:
                 raise siptrackdlib.errors.SiptrackError('old password must be supplied')
-            old_password = self.session.user.password
+            old_password = session.user.password
         user.setPassword(new_password, old_password)
         # Update the sessions stored password if we're
         # changing our own password.
         if session_user.oid == user.oid:
-            self.session.user.password = new_password
+            session.user.password = new_password
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_reset_password(self, oid, password):
-        session_user = self.session.user.user
-        user = self.getOID(oid)
+    @helpers.ValidateSession()
+    def xmlrpc_reset_password(self, session, oid, password):
+        session_user = session.user.user
+        user = self.getOID(session, oid)
         if session_user.administrator != True and \
                 session_user.oid != user.oid:
             raise errors.PermissionDenied()
@@ -216,63 +174,55 @@ class BaseUserRPC(baserpc.BaseRPC):
         # Update the sessions stored password if we're
         # changing our own password.
         if session_user.oid == user.oid:
-            self.session.user.password = password
+            session.user.password = password
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_administrator(self, oid, value):
-        user = self.getOID(oid)
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_administrator(self, session, oid, value):
+        user = self.getOID(session, oid)
         user.administrator = value
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_connect_password_key(self, user_oid, pk_oid,
+    @helpers.ValidateSession()
+    def xmlrpc_connect_password_key(self, session, user_oid, pk_oid,
             user_password = False, pk_password = False):
         """Connect the user to a password key.
         """
-        user = self.getOID(user_oid)
-        session_user = self.session.user.user
+        user = self.getOID(session, user_oid)
+        session_user = session.user.user
         if session_user.administrator != True and \
                 session_user.oid != user.oid:
             raise errors.PermissionDenied()
         if user_password is False:
-            if user is self.session.user.user:
-                user_password = self.session.user.password
-        pk = self.object_store.getOID(pk_oid, 'password key', user = self.user)
+            if user is session.user.user:
+                user_password = session.user.password
+        pk = self.object_store.getOID(pk_oid, 'password key', user = session.user)
         user.connectPasswordKey(pk, user_password, pk_password)
         return True
 
 class UserLocalRPC(BaseUserRPC):
     node_type = 'user local'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_add(self, parent_oid, username, password, administrator):
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_add(self, session, parent_oid, username, password, administrator):
         """Create a new User."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'user local', username, password,
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'user local', username, password,
                 administrator)
         return obj.oid
 
 class UserLDAPRPC(BaseUserRPC):
     node_type = 'user ldap'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
+    @helpers.ValidateSession(require_admin=True)
     def xmlrpc_add(self, *args, **kwargs):
         """Create a new User."""
         raise siptrackdlib.errors.SiptrackError('add not supported for ldap users')
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_set_password(self, oid, new_password, old_password):
-        session_user = self.session.user.user
-        user = self.getOID(oid)
+    @helpers.ValidateSession()
+    def xmlrpc_set_password(self, session, oid, new_password, old_password):
+        session_user = session.user.user
+        user = self.getOID(session, oid)
         if session_user.administrator != True and \
                 session_user.oid != user.oid:
             raise errors.PermissionDenied()
@@ -284,18 +234,15 @@ class UserLDAPRPC(BaseUserRPC):
 class UserActiveDirectoryRPC(BaseUserRPC):
     node_type = 'user active directory'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
+    @helpers.ValidateSession(require_admin=True)
     def xmlrpc_add(self, *args, **kwargs):
         """Create a new User."""
         raise siptrackdlib.errors.SiptrackError('add not supported for active directory users')
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_set_password(self, oid, new_password, old_password):
-        session_user = self.session.user.user
-        user = self.getOID(oid)
+    @helpers.ValidateSession()
+    def xmlrpc_set_password(self, session, oid, new_password, old_password):
+        session_user = session.user.user
+        user = self.getOID(session, oid)
         if session_user.administrator != True and \
                 session_user.oid != user.oid:
             raise errors.PermissionDenied()
@@ -307,43 +254,35 @@ class UserActiveDirectoryRPC(BaseUserRPC):
 class UserGroupRPC(baserpc.BaseRPC):
     node_type = 'user group'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_add(self, parent_oid, users):
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_add(self, session, parent_oid, users):
         """Create a new UserGroup."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        users = [self.object_store.getOID(oid, user = self.user) for oid in \
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        users = [self.object_store.getOID(oid, user = session.user) for oid in \
                 users]
-        obj = parent.add(self.user, 'user group', users)
+        obj = parent.add(session.user, 'user group', users)
         return obj.oid
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_set_users(self, oid, users):
-        node = self.getOID(oid)
-        users = [self.object_store.getOID(oid, ['user local', 'user ldap'], user = self.user) for oid in users]
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_set_users(self, session, oid, users):
+        node = self.getOID(session, oid)
+        users = [self.object_store.getOID(oid, ['user local', 'user ldap'], user = session.user) for oid in users]
         node.users.set(users)
         return True
 
 class UserGroupLDAPRPC(baserpc.BaseRPC):
     node_type = 'user group ldap'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_add(self, parent_oid, users):
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_add(self, session, parent_oid, users):
         """Don't create a new UserGroup."""
         raise siptrackdlib.errors.SiptrackError('add not supported for LDAP groups')
 
 class UserGroupActiveDirectoryRPC(baserpc.BaseRPC):
     node_type = 'user group ldap'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    @helpers.require_admin
-    def xmlrpc_add(self, parent_oid, users):
+    @helpers.ValidateSession(require_admin=True)
+    def xmlrpc_add(self, session, parent_oid, users):
         """Don't create a new UserGroup."""
         raise siptrackdlib.errors.SiptrackError('add not supported for active directory groups')
 
