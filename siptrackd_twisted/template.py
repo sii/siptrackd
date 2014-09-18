3ad847e176f8fdc2b86f0f486652b4455ac502c1
Simon Ekstrand <simon@ipeer.se>
2014-09-18 13:06:41 +0200

    Updated xmlrpc session validation for template.py

--- siptrackd_twisted/template.py
+++ siptrackd_twisted/template.py
@@ -9,49 +9,45 @@ from siptrackd_twisted import errors
 from siptrackd_twisted import baserpc
 
 class TemplateRPC(baserpc.BaseRPC):
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_suggest_templates(self, base_oid, template_class):
+    @helpers.ValidateSession()
+    def xmlrpc_suggest_templates(self, session, base_oid, template_class):
         """Suggest available device templates for a node."""
-        base_node = self.object_store.getOID(base_oid, user = self.user)
+        base_node = self.object_store.getOID(base_oid, user = session.user)
         oids = [tmpl.oid for tmpl in \
                 template.suggest_templates(base_node, template_class)]
         return oids
 
 class BaseTemplateRPC(baserpc.BaseRPC):
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, inheritance_only,
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, inheritance_only,
             inherited_templates):
         """Create a new template."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        inherited_templates = [self.object_store.getOID(oid, user = self.user) for oid in \
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        inherited_templates = [self.object_store.getOID(oid, user = session.user) for oid in \
                 inherited_templates]
-        obj = parent.add(self.user, self.node_type, inheritance_only,
+        obj = parent.add(session.user, self.node_type, inheritance_only,
                 inherited_templates)
         return obj.oid
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_apply(self, oid, apply_oid, arguments = {}, overwrite = False,
+    @helpers.ValidateSession()
+    def xmlrpc_apply(self, session, oid, apply_oid, arguments = {}, overwrite = False,
             skip_rules = []):
         """Apply a template to a node."""
-        template = self.getOID(oid)
-        apply_node = self.object_store.getOID(apply_oid, user = self.user)
-        skip_rules = [self.object_store.getOID(rule, user = self.user) for rule in skip_rules]
+        template = self.getOID(session, oid)
+        apply_node = self.object_store.getOID(apply_oid, user = session.user)
+        skip_rules = [self.object_store.getOID(rule, user = session.user) for rule in skip_rules]
         # Disable threaded version, for now.
-        template.apply(apply_node, arguments, overwrite, skip_rules, self.user)
+        template.apply(apply_node, arguments, overwrite, skip_rules, session.user)
         return True
-#        user = self.user
+#        user = session.user
 #        return threads.deferToThread(template.apply, apply_node, arguments, overwrite, skip_rules, user)
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_combined_rules(self, oid):
+    @helpers.ValidateSession()
+    def xmlrpc_combined_rules(self, session, oid):
         """Return a list of combined rules for a template."""
-        template = self.getOID(oid)
+        template = self.getOID(session, oid)
         match_oids = []
-        listcreator = gatherer.ListCreator(self.object_store, self.user)
+        listcreator = gatherer.ListCreator(self.object_store, session.user)
         for rule in template.listCombinedRules():
             listcreator.build(rule, 0, include_parents = True,
                 include_associations = False, include_references = False)
@@ -59,27 +55,24 @@ class BaseTemplateRPC(baserpc.BaseRPC):
 
         return [listcreator.prepared_data, match_oids]
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_suggest_templates(self, base_oid):
+    @helpers.ValidateSession()
+    def xmlrpc_suggest_templates(self, session, base_oid):
         """Suggest available device templates for a node."""
-        base_node = self.object_store.getOID(base_oid, user = self.user)
+        base_node = self.object_store.getOID(base_oid, user = session.user)
         oids = [tmpl.oid for tmpl in \
                 template.suggest_templates(base_node, self.node_type)]
         return oids
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_set_inheritance_only(self, oid, inheritance_only):
-        tmpl = self.getOID(oid)
+    @helpers.ValidateSession()
+    def xmlrpc_set_inheritance_only(self, session, oid, inheritance_only):
+        tmpl = self.getOID(session, oid)
         tmpl.inheritance_only.set(inheritance_only)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_set_inherited_templates(self, oid, inherited_templates):
-        node = self.getOID(oid)
-        inherited_templates = [self.object_store.getOID(oid, user = self.user) for oid in inherited_templates]
+    @helpers.ValidateSession()
+    def xmlrpc_set_inherited_templates(self, session, oid, inherited_templates):
+        node = self.getOID(session, oid)
+        inherited_templates = [self.object_store.getOID(oid, user = session.user) for oid in inherited_templates]
         node.inherited_templates.set(inherited_templates)
         return True
 
@@ -95,185 +88,169 @@ class TemplateRuleRPC(baserpc.BaseRPC):
 class TemplateRulePasswordRPC(baserpc.BaseRPC):
     node_type = 'template rule password'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, username, description, key_oid):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, username, description, key_oid):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, 'template', user = self.user)
+        parent = self.object_store.getOID(parent_oid, 'template', user = session.user)
         key = None
         if len(key_oid) > 0:
-            key = self.object_store.getOID(key_oid, 'password key', user = self.user)
-        obj = parent.add(self.user, 'template rule password', username, description, key)
+            key = self.object_store.getOID(key_oid, 'password key', user = session.user)
+        obj = parent.add(session.user, 'template rule password', username, description, key)
         return obj.oid
 
 class TemplateRuleAssignNetworkRPC(baserpc.BaseRPC):
     node_type = 'template rule assign network'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, 'template', user = self.user)
-        obj = parent.add(self.user, 'template rule assign network')
+        parent = self.object_store.getOID(parent_oid, 'template', user = session.user)
+        obj = parent.add(session.user, 'template rule assign network')
         return obj.oid
 
 class TemplateRuleSubdeviceRPC(baserpc.BaseRPC):
     node_type = 'template rule subdevice'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, num_devices, device_template_oid,
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, num_devices, device_template_oid,
             sequence_offset):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
+        parent = self.object_store.getOID(parent_oid, user = session.user)
         device_template = None
         if device_template_oid:
             device_template = self.object_store.getOID(device_template_oid,
-                    ['device template'], user = self.user)
-        obj = parent.add(self.user, 'template rule subdevice', num_devices,
+                    ['device template'], user = session.user)
+        obj = parent.add(session.user, 'template rule subdevice', num_devices,
             device_template, sequence_offset)
         return obj.oid
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_set_num_devices(self, oid, num_devices):
-        tmpl = self.getOID(oid)
+    @helpers.ValidateSession()
+    def xmlrpc_set_num_devices(self, session, oid, num_devices):
+        tmpl = self.getOID(session, oid)
         tmpl.num_devices.set(num_devices)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_set_device_template(self, oid, device_template_oid):
-        rule = self.getOID(oid)
+    @helpers.ValidateSession()
+    def xmlrpc_set_device_template(self, session, oid, device_template_oid):
+        rule = self.getOID(session, oid)
         tmpl = None
         if device_template_oid:
             tmpl = self.object_store.getOID(device_template_oid,
-                    'template', user = self.user)
+                    'template', user = session.user)
         rule.device_template.set(tmpl)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_set_sequence_offset(self, oid, sequence_offset):
-        tmpl = self.getOID(oid)
+    @helpers.ValidateSession()
+    def xmlrpc_set_sequence_offset(self, session, oid, sequence_offset):
+        tmpl = self.getOID(session, oid)
         tmpl.sequence_offset.set(sequence_offset)
         return True
 
 class TemplateRuleTextRPC(baserpc.BaseRPC):
     node_type = 'template rule text'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, attr_name, versions):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, attr_name, versions):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'template rule text', attr_name, versions)
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'template rule text', attr_name, versions)
         return obj.oid
 
 class TemplateRuleFixedRPC(baserpc.BaseRPC):
     node_type = 'template rule fixed'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, attr_name, value, variable_expansion,
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, attr_name, value, variable_expansion,
             versions):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'template rule fixed', attr_name, value,
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'template rule fixed', attr_name, value,
                 variable_expansion, versions)
         return obj.oid
 
 class TemplateRuleRegmatchRPC(baserpc.BaseRPC):
     node_type = 'template rule regmatch'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, attr_name, regexp, versions):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, attr_name, regexp, versions):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'template rule regmatch', attr_name, regexp, versions)
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'template rule regmatch', attr_name, regexp, versions)
         return obj.oid
 
 class TemplateRuleBoolRPC(baserpc.BaseRPC):
     node_type = 'template rule bool'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, attr_name, default_value, versions):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, attr_name, default_value, versions):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'template rule bool', attr_name, default_value,
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'template rule bool', attr_name, default_value,
                 versions)
         return obj.oid
 
 class TemplateRuleIntRPC(baserpc.BaseRPC):
     node_type = 'template rule int'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, attr_name, default_value, versions):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, attr_name, default_value, versions):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'template rule int', attr_name, default_value,
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'template rule int', attr_name, default_value,
                 versions)
         return obj.oid
 
 class TemplateRulePasswordRPC(baserpc.BaseRPC):
     node_type = 'template rule password'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, username, description, key_oid):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, username, description, key_oid):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
+        parent = self.object_store.getOID(parent_oid, user = session.user)
         key = None
         if len(key_oid) > 0:
-            key = self.object_store.getOID(key_oid, user = self.user)
-        obj = parent.add(self.user, 'template rule password', username, description, key)
+            key = self.object_store.getOID(key_oid, user = session.user)
+        obj = parent.add(session.user, 'template rule password', username, description, key)
         return obj.oid
 
 class TemplateRuleAssignNetworkRPC(baserpc.BaseRPC):
     node_type = 'template rule assign network'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'template rule assign network')
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'template rule assign network')
         return obj.oid
 
 class TemplateRuleDeleteAttributeRPC(baserpc.BaseRPC):
     node_type = 'template rule delete attribute'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, attr_name):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, attr_name):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'template rule delete attribute', attr_name)
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'template rule delete attribute', attr_name)
         return obj.oid
 
 class TemplateRuleFlushNodesRPC(baserpc.BaseRPC):
     node_type = 'template rule flush nodes'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, include, exclude):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, include, exclude):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'template rule flush nodes', include, exclude)
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'template rule flush nodes', include, exclude)
         return obj.oid
 
 class TemplateRuleFlushAssociationsRPC(baserpc.BaseRPC):
     node_type = 'template rule flush associations'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid, include, exclude):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid, include, exclude):
         """Create a new template rule."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'template rule flush associations', include, exclude)
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'template rule flush associations', include, exclude)
         return obj.oid
 
 def template_data_extractor(node, user):
