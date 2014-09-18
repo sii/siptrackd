d8f2f10ea447e98459b8e7f03772067dac5e8668
Simon Ekstrand <simon@ipeer.se>
2014-09-18 13:05:58 +0200

    Updated xmlrpc session validation for device.py

--- siptrackd_twisted/device.py
+++ siptrackd_twisted/device.py
@@ -10,51 +10,46 @@ from siptrackd_twisted import baserpc
 class DeviceRPC(baserpc.BaseRPC):
     node_type = 'device'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid):
         """Create a new device."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        device = parent.add(self.user, 'device')
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        device = parent.add(session.user, 'device')
         return device.oid
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_delete(self, oid, prune_networks = True):
+    @helpers.ValidateSession()
+    def xmlrpc_delete(self, session, oid, prune_networks = True):
         """Delete a device."""
-        device = self.getOID(oid)
-        device.delete(recursive = True, user = self.user,
+        device = self.getOID(session, oid)
+        device.delete(recursive = True, user = session.user,
                 prune_networks = prune_networks)
         return True
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_autoassign_network(self, oid):
+    @helpers.ValidateSession()
+    def xmlrpc_autoassign_network(self, session, oid):
         """Autoassign a network to a device."""
-        device = self.getOID(oid)
-        network = device.autoAssignNetwork(self.user)
+        device = self.getOID(session, oid)
+        network = device.autoAssignNetwork(session.user)
         return network.oid
 
 class DeviceTreeRPC(baserpc.BaseRPC):
     node_type = 'device tree'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid):
         """Create a new device tree."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'device tree')
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'device tree')
         return obj.oid
 
 class DeviceCategoryRPC(baserpc.BaseRPC):
     node_type = 'device category'
 
-    @helpers.error_handler
-    @helpers.validate_session
-    def xmlrpc_add(self, parent_oid):
+    @helpers.ValidateSession()
+    def xmlrpc_add(self, session, parent_oid):
         """Create a new device category."""
-        parent = self.object_store.getOID(parent_oid, user = self.user)
-        obj = parent.add(self.user, 'device category')
+        parent = self.object_store.getOID(parent_oid, user = session.user)
+        obj = parent.add(session.user, 'device category')
         return obj.oid
 
 gatherer.node_data_registry.register(device.Device,
