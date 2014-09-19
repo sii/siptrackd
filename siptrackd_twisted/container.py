0d6b9681903bad2d3d1066901cb24ac1610218d5
Simon Ekstrand <simon@ipeer.se>
2014-09-19 13:23:57 +0200

    Use commit in xmlrpc container.py.

--- siptrackd_twisted/container.py
+++ siptrackd_twisted/container.py
@@ -1,4 +1,5 @@
 from twisted.web import xmlrpc
+from twisted.internet import defer
 import xmlrpclib
 
 from siptrackdlib import container
@@ -11,22 +12,26 @@ from siptrackd_twisted import baserpc
 class ContainerTreeRPC(baserpc.BaseRPC):
     node_type = 'container tree'
 
+    @defer.inlineCallbacks
     @helpers.ValidateSession()
     def xmlrpc_add(self, session, parent_oid):
         """Create a new container tree."""
         parent = self.object_store.getOID(parent_oid, user = session.user)
         obj = parent.add(session.user, 'container tree')
-        return obj.oid
+        yield self.object_store.commit(obj)
+        defer.returnValue(obj.oid)
 
 class ContainerRPC(baserpc.BaseRPC):
     node_type = 'container'
 
+    @defer.inlineCallbacks
     @helpers.ValidateSession()
     def xmlrpc_add(self, session, parent_oid):
         """Create a new container."""
         parent = self.object_store.getOID(parent_oid, user = session.user)
         obj = parent.add(session.user, 'container')
-        return obj.oid
+        yield self.object_store.commit(obj)
+        defer.returnValue(obj.oid)
 
 gatherer.node_data_registry.register(container.ContainerTree,
         gatherer.no_data_extractor)
